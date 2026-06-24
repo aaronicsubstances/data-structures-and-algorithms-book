437768b3ea589a7bd44d52941ee567589f49261a
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2026-06-24 08:51:00 +0000

    Bug fixes for new solutions introduced for chapter 12 exercises

--- 12 binary_trees/answers_to_questions/12-10-rebuild_tree.js
+++ 12 binary_trees/answers_to_questions/12-10-rebuild_tree.js
@@ -1,19 +1,40 @@
-const rebuild = (inOrder, preOrder) => {
+const { newNode, newBinaryTree, print } = require("../binary_search_tree");
 
-	const root = preOrder[0]
-	const rootIndex = inOrder.findIndex(root)
-	const leftPreOrder = []
-	const rightPreOrder = []
+const rebuild = (inOrder, preOrder) => {
+	if (!inOrder.length) {
+		return newBinaryTree();
+	}
+
+	const root = preOrder[0];
+	const rootIndex = inOrder.indexOf(root);
+	const leftPreOrder = [];
+	const rightPreOrder = [];
 	for (const n of preOrder.slice(1)) {
-		const idx = inOrder.findIndex(n)
+		const idx = inOrder.indexOf(n);
 		if (idx < rootIndex) {
-			leftPreOrder.push(n)
+			leftPreOrder.push(n);
 		}
 		else {
-			rightPreOrder.push(n)
+			rightPreOrder.push(n);
 		}
 	}
-	const left = rebuild(inOrder.slice(0, rootIndex), leftPreOrder)
-	const right = rebuild(inOrder.slice(rootIndex + 1), rightPreOrder)
-	return newNode(root, left, right)
+	const left = rebuild(inOrder.slice(0, rootIndex), leftPreOrder);
+	const right = rebuild(inOrder.slice(rootIndex + 1), rightPreOrder);
+	return newNode(root, left, right);
 }
+
+let example = rebuild([], []);
+print(example); // (empty)
+
+example = rebuild([1], [1]);
+print(example); // 1
+
+example = rebuild([1, 2], [2, 1]);
+print(example); // 2\n L: 1
+
+example = rebuild([1, 2], [1, 2]);
+print(example); // 1\n R: 2
+
+example = rebuild([4, 9, 10, 11, 12, 22, 23, 24, 56, 60],
+	[22, 9, 4, 11, 10, 12, 60, 24, 23, 56]);
+print(example);
--- 12 binary_trees/answers_to_questions/12-11-more_rebuilding.js
+++ 12 binary_trees/answers_to_questions/12-11-more_rebuilding.js
@@ -1,19 +1,40 @@
-const rebuild = (inOrder, postOrder) => {
+const { newNode, newBinaryTree, print } = require("../binary_search_tree");
 
-	const root = postOrder[postOrder.length -1]
-	const rootIndex = inOrder.findIndex(root)
-	const leftPostOrder = []
-	const rightPostOrder = []
+const rebuild = (inOrder, postOrder) => {
+	if (!inOrder.length) {
+		return newBinaryTree();
+	}
+
+	const root = postOrder[postOrder.length - 1];
+	const rootIndex = inOrder.indexOf(root);
+	const leftPostOrder = [];
+	const rightPostOrder = [];
 	for (const n of postOrder.slice(0, postOrder.length - 1)) {
-		const idx = inOrder.findIndex(n)
+		const idx = inOrder.indexOf(n);
 		if (idx < rootIndex) {
-			leftPostOrder.push(n)
+			leftPostOrder.push(n);
 		}
 		else {
-			rightPostOrder.push(n)
+			rightPostOrder.push(n);
 		}
 	}
-	const left = rebuild(inOrder.slice(0, rootIndex), leftPostOrder)
-	const right = rebuild(inOrder.slice(rootIndex + 1), rightPostOrder)
-	return newNode(root, left, right)
+	const left = rebuild(inOrder.slice(0, rootIndex), leftPostOrder);
+	const right = rebuild(inOrder.slice(rootIndex + 1), rightPostOrder);
+	return newNode(root, left, right);
 }
+
+let example = rebuild([], []);
+print(example); // (empty)
+
+example = rebuild([1], [1]);
+print(example); // 1
+
+example = rebuild([1, 2], [2, 1]);
+print(example); // 1\n R: 2
+
+example = rebuild([1, 2], [1, 2]);
+print(example); // 2\n L: 1
+
+example = rebuild([4, 9, 10, 11, 12, 22, 23, 24, 56, 60],
+	[4, 10, 12, 11, 9, 23, 56, 24, 60, 22]);
+print(example);
